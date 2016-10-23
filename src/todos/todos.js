670f664208bc740ab32dbb1419aa7e99005cf9dc
Ksquare Inc <ksquareinc@Ksquares-MacBook-Pro-10.local>
2016-10-23 17:26:25 -0500

    working on edit functionality

--- src/todos/todos.js
+++ src/todos/todos.js
@@ -1,39 +1,57 @@
-export default function($scope) {
+export default function ($scope) {
 	let params = {
 		createHasInput: false
 	};
-	
+
 	$scope.todos = [
 		{
-		task: 'do dishes',
-		isCompleted: false
+			task: 'do dishes',
+			isCompleted: false,
+			isEditing: false
 		},
 		{
-			task:'walk the dog',
-			isCompleted: true
+			task: 'walk the dog',
+			isCompleted: true,
+			isEditing: false
 		}
 	];
-	
+
 	$scope.onCompletedClick = todo => {
-		todo.isCompleted= !todo.isCompleted;
+		todo.isCompleted = !todo.isCompleted;
 	};
-// Resets input form with the ng-submit functionality	
+//edit button functionality
+	$scope.onEditClick = todo => {
+		todo.isEditing = true;
+		todo.updatedTask = todo.task;
+		};
+	
+	$scope.onCancelClick = todo => {
+		todo.isEditing = false;
+	}
+		// Resets input form with the ng-submit functionality	
 	$scope.createTask = () => {
 		params.createHasInput = false;
-		$scope.createTaskInput = '';	
+		$scope.createTaskInput = '';
 	};
 	
+	$scope.updateTask = todo => {
+		todo.task = todo.updatedTask;
+		todo.isEditing = false;
+	}
+
 	$scope.$watch('createTaskInput', val => {
-		if(!val && params.createHasInput) {
+		if (!val && params.createHasInput) {
 			$scope.todos.pop();
 			params.createHasInput = false;
-		}
-			else if (val && !params.createHasInput) {
-				$scope.todos.push({task: val, isCompleted: false});
-				params.createHasInput = true;
-	} else if (val && params.createHasInput) {
+		} else if (val && !params.createHasInput) {
+			$scope.todos.push({
+				task: val,
+				isCompleted: false
+			});
+			params.createHasInput = true;
+		} else if (val && params.createHasInput) {
 			$scope.todos[$scope.todos.length - 1].task = val
-	}																			 
-																						 
-});
+		}
+
+	});
 }
