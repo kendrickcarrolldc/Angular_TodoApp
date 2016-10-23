f347742b972feb8f19a4a369dcccece3f7a81a02
Ksquare Inc <ksquareinc@Ksquares-MacBook-Pro-10.local>
2016-10-23 11:45:06 -0500

    working on new app

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+
+var PORT = process.env.PORT || 3000;
+
+app.all('/*', function(req, res) {
+  res.send('\
+  <!DOCTYPE html>\
+		<html>\
+			<title> ToDo App</title>\
+			<base href="/">\
+		</head>\
+		<body>\
+		<div ui-view></div>\
+			<script src="bundle.js"></script>\
+</body>\
+</html>\
+');
+});
+
+app.listen(PORT, function() {
+  console.log('Server running on', + PORT);
+});
