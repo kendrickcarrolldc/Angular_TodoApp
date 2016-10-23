a8eaa733818bdce65ffd2561d7c71aa3044723f9
Ksquare Inc <ksquareinc@Ksquares-MacBook-Pro-10.local>
2016-10-23 15:51:13 -0500

    reworking functionality for notes

---
+++ src/todos/todos.js
@@ -0,0 +1,39 @@
+export default function($scope) {
+	let params = {
+		createHasInput: false
+	};
+	
+	$scope.todos = [
+		{
+		task: 'do dishes',
+		isCompleted: false
+		},
+		{
+			task:'walk the dog',
+			isCompleted: true
+		}
+	];
+	
+	$scope.onCompletedClick = todo => {
+		todo.isCompleted= !todo.isCompleted;
+	};
+// Resets input form with the ng-submit functionality	
+	$scope.createTask = () => {
+		params.createHasInput = false;
+		$scope.createTaskInput = '';	
+	};
+	
+	$scope.$watch('createTaskInput', val => {
+		if(!val && params.createHasInput) {
+			$scope.todos.pop();
+			params.createHasInput = false;
+		}
+			else if (val && !params.createHasInput) {
+				$scope.todos.push({task: val, isCompleted: false});
+				params.createHasInput = true;
+	} else if (val && params.createHasInput) {
+			$scope.todos[$scope.todos.length - 1].task = val
+	}																			 
+																						 
+});
+}
